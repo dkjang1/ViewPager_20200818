97ba2afb5aeb681412a7504ece49ad3add99c4d1
Jinkyeong96 <69335959+Jinkyeong96@users.noreply.github.com>
2020-08-18 20:56:09 +0900

    뷰페이저 완성

--- app/src/main/java/com/kakao/viewpager_20200818/MainActivity.kt
+++ app/src/main/java/com/kakao/viewpager_20200818/MainActivity.kt
@@ -2,8 +2,13 @@ package com.kakao.viewpager_20200818
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kakao.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    lateinit var mMainViewPagerAdapter: MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +21,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        mMainViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mMainViewPagerAdapter
     }
 }
