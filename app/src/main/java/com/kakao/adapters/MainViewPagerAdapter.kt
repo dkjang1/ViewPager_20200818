c8aae63cd62cb2b5c07dded8ad52c75d068478f1
Jinkyeong96 <69335959+Jinkyeong96@users.noreply.github.com>
2020-08-18 21:46:57 +0900

    return when 이용예시

--- app/src/main/java/com/kakao/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/kakao/adapters/MainViewPagerAdapter.kt
@@ -10,6 +10,12 @@ import com.kakao.fragments.PhoneNumFragment
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0 -> "이름"
+            1 -> "휴대폰번호"
+            else -> "거주지"
+        }
+/*
         if (position == 0) {
             return "이름"
         } else if (position == 1) {
@@ -17,10 +23,17 @@ class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
         } else {
             return "거주지"
         }
+ */
 //        return super.getPageTitle(position)
     }
 
     override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> NameFragment()
+            1 -> PhoneNumFragment()
+            else -> AddressFragment()
+        }
+/*
         if (position == 0) {
             return NameFragment()
         } else if (position == 1) {
@@ -28,6 +41,7 @@ class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
         } else {
             return AddressFragment()
         }
+ */
     }
 
     override fun getCount(): Int {
